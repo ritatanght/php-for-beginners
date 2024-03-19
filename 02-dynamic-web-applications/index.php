c522bcd484fac153fe492728ecb788cbb360a020
Rita Tang <doggymint@gmail.com>
2024-03-18 21:42:56 -0400

    Connect to local mysql database

--- 02-dynamic-web-applications/index.php
+++ 02-dynamic-web-applications/index.php
@@ -2,6 +2,18 @@
 
 require 'functions.php';
 
-$heading = 'Home';
+// require 'router.php';
 
-require "views/index.view.php";
+// connect to database
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo ("<li>" . $post['title'] . "</li>");
+}
