b768d7012d2e2ccb62c0e200ed5efd52c3d44c32
Rita Tang <doggymint@gmail.com>
2024-03-19 14:08:21 -0400

    Add params to db query and get id param from query string

--- 02-dynamic-web-applications/Database.php
+++ 02-dynamic-web-applications/Database.php
@@ -11,13 +11,13 @@ class Database
     $this->connection = new PDO($dsn, $username, $password, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
   }
 
-  public function query($query)
+  public function query($query, $params = [])
   {
 
 
     $statement = $this->connection->prepare($query);
 
-    $statement->execute();
+    $statement->execute($params);
 
     return $statement;
   }
--- 02-dynamic-web-applications/index.php
+++ 02-dynamic-web-applications/index.php
@@ -8,7 +8,11 @@ $config = require 'config.php';
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+$query = "select * from posts where id = ?";
+
+$posts = $db->query($query, [$id])->fetchAll();
 
 
 foreach ($posts as $post) {
