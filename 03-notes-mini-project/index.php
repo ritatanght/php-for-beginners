5ced8813a53060710fde8eef227e95e68613ee4a
Rita Tang <doggymint@gmail.com>
2024-03-20 17:43:06 -0400

    Add Response class and require it in index

--- 03-notes-mini-project/index.php
+++ 03-notes-mini-project/index.php
@@ -2,15 +2,5 @@
 
 require 'functions.php';
 require 'Database.php';
+require 'Response.php';
 require 'router.php';
-
-// $id = $_GET['id'];
-
-// $query = "select * from posts where id = ?";
-
-// $posts = $db->query($query, [$id])->fetchAll();
-
-
-// foreach ($posts as $post) {
-//   echo ("<li>" . $post['title'] . "</li>");
-// }
