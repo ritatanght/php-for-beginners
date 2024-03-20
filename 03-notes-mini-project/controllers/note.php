bac8ce9c258bdcaceb1e298a1e0026c0cf5366d0
Rita Tang <doggymint@gmail.com>
2024-03-20 17:44:05 -0400

    Add 403 view and display it when user tries to read others' notes

--- 03-notes-mini-project/controllers/note.php
+++ 03-notes-mini-project/controllers/note.php
@@ -4,7 +4,17 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+
+if (!$note) {
+  abort();
+}
+
+// unauthorized access
+if ($note['user_id'] != $currentUserId) {
+  abort(Response::FORBIDDEN);
+}
 
 require 'views/note.view.php';
