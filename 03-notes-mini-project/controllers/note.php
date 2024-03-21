d82052ae12773529540ad88d729dff72f5af8e5e
Rita Tang <doggymint@gmail.com>
2024-03-20 20:35:15 -0400

    Add get method to Database for fetching multiple records

--- 03-notes-mini-project/controllers/note.php
+++ 03-notes-mini-project/controllers/note.php
@@ -4,14 +4,10 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Note';
-$currentUserId = 1;
+$currentUserId = '1';
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-
-// unauthorized access
-if ($note['user_id'] != $currentUserId) {
-  abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require 'views/note.view.php';
