3acc479a406e95a254f3f298a7c835b8c700ecda
Rita Tang <doggymint@gmail.com>
2024-03-23 18:23:45 -0400

    Restructure files and update file name to follow REST naming conventions

--- 04-project-organization/views/notes/show.view.php
+++ 04-project-organization/views/notes/show.view.php
@@ -1,6 +1,6 @@
-<?php require('partials/head.php'); ?>
-<?php require('partials/nav.php'); ?>
-<?php require('partials/banner.php'); ?>
+<?php require('views/partials/head.php'); ?>
+<?php require('views/partials/nav.php'); ?>
+<?php require('views/partials/banner.php'); ?>
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
     <p class="mb-6"><a href="/notes" class="text-blue-500 underline">Go back...</a></p>
@@ -10,4 +10,4 @@
 
   </div>
 </main>
-<?php require('partials/footer.php'); ?>
+<?php require('views/partials/footer.php'); ?>
