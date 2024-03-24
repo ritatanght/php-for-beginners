e2cb90761fbf63074790bbf1721f42a920a60875
Rita Tang <doggymint@gmail.com>
2024-03-24 12:02:42 -0400

    Move classes into Core/ and update the paths

--- 04-project-organization/public/index.php
+++ 04-project-organization/public/index.php
@@ -3,11 +3,9 @@
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php';
-// require base_path('Database.php');
-// require base_path('Response.php');
 
 spl_autoload_register(function ($class) {
-  require base_path($class . '.php');
+  require base_path("Core/{$class}.php");
 });
 
 require base_path('router.php');
