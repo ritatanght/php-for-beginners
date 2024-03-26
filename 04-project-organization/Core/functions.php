db3f9cec23d4acc1ff558450da4727221315585c
Rita Tang <doggymint@gmail.com>
2024-03-25 22:21:48 -0400

    Add Router class and update code to use the new router

--- 04-project-organization/Core/functions.php
+++ 04-project-organization/Core/functions.php
@@ -16,6 +16,13 @@ function urlIs($value)
   return $_SERVER['REQUEST_URI'] === $value;
 }
 
+function abort($code = 404)
+{
+  http_response_code($code);
+  view("{$code}.php");
+  die();
+}
+
 function authorize($condition, $status = Response::FORBIDDEN)
 {
   if (!$condition) {
