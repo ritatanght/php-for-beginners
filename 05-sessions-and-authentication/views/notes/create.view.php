24e7421db58b3fdb9bbe351a59d2688049aab636
Rita Tang <doggymint@gmail.com>
2024-03-28 21:17:29 -0400

    Add register page and show login status on nav bar

--- 05-sessions-and-authentication/views/notes/create.view.php
+++ 05-sessions-and-authentication/views/notes/create.view.php
@@ -20,7 +20,6 @@
       </div>
 
       <div class="mt-6 flex items-center justify-end gap-x-6">
-
         <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
       </div>
     </form>
