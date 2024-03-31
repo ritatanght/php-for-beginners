bb2ad685b6bca69e8e486ee2a69744ed81382b8b
Rita Tang <doggymint@gmail.com>
2024-03-30 20:27:48 -0400

    Revise file name to capitalized

--- 05-sessions-and-authentication/Core/Router.php
+++ 05-sessions-and-authentication/Core/Router.php
@@ -6,49 +6,50 @@ class Router
 {
   protected $routes = [];
 
-  public function get($uri, $controller)
+  public function add($method, $uri, $controller)
   {
     $this->routes[] = [
       'uri' => $uri,
       'controller' => $controller,
-      'method' => 'GET'
+      'method' => $method,
+      'middleware' => null
     ];
+
+    return $this;
+  }
+
+  public function get($uri, $controller)
+  {
+    return $this->add('GET', $uri, $controller);
   }
 
   public function post($uri, $controller)
   {
-    $this->routes[] = [
-      'uri' => $uri,
-      'controller' => $controller,
-      'method' => 'POST'
-    ];
+    return $this->add('POST', $uri, $controller);
   }
 
   public function delete($uri, $controller)
   {
-    $this->routes[] = [
-      'uri' => $uri,
-      'controller' => $controller,
-      'method' => 'DELETE'
-    ];
+    return $this->add('DELETE', $uri, $controller);
   }
 
   public function patch($uri, $controller)
   {
-    $this->routes[] = [
-      'uri' => $uri,
-      'controller' => $controller,
-      'method' => 'PATCH'
-    ];
+    return $this->add('PATCH', $uri, $controller);
   }
 
   public function put($uri, $controller)
   {
-    $this->routes[] = [
-      'uri' => $uri,
-      'controller' => $controller,
-      'method' => 'PUT'
-    ];
+    return $this->add('PUT', $uri, $controller);
+  }
+
+  public function only($key)
+  {
+    dd($key);
+    //end($this->routes)['middleware'] = $key;
+    // $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+    // return $this;
   }
 
   public function route($uri, $method)
