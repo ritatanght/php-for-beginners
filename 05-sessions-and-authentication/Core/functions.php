fad573ea18ef31a79255c4190be8a6e5918c38e7
Rita Tang <doggymint@gmail.com>
2024-03-31 20:56:22 -0400

    Add logout helper function

--- 05-sessions-and-authentication/Core/functions.php
+++ 05-sessions-and-authentication/Core/functions.php
@@ -46,4 +46,16 @@ function login($user)
   $_SESSION['user'] = [
     'email' => $user['email']
   ];
+
+  session_regenerate_id(true);
+}
+
+function logout()
+{
+  $_SESSION = []; // clear out the super global
+  session_destroy(); // destroy the session file
+
+  // delete the cookie
+  $params = session_get_cookie_params();
+  setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain']);
 }
